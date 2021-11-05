2ed9440dfaadefb320fc253fc5ad52361369f11f
jyoo0515 <jasonyoo0515@gmail.com>
2021-11-06 03:08:19 +0900

    Update users functionality

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,38 +1,45 @@
-import { ApiProperty } from "@nestjs/swagger";
-import { BaseEntity, BeforeInsert, BeforeUpdate, Column, Entity, PrimaryColumn } from "typeorm";
+import { ApiProperty } from '@nestjs/swagger';
+import {
+  BaseEntity,
+  BeforeInsert,
+  BeforeUpdate,
+  Column,
+  Entity,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
 import * as bcrypt from 'bcrypt';
 
 @Entity({ name: 'users' })
 export class User extends BaseEntity {
-    @ApiProperty()
-    @PrimaryColumn()
-    id: number;
+  @ApiProperty()
+  @PrimaryGeneratedColumn()
+  id: number;
 
-    @ApiProperty()
-    @Column()
-    name: string;
+  @ApiProperty()
+  @Column()
+  name: string;
 
-    @ApiProperty()
-    @Column({ unique: true })
-    username: string;
+  @ApiProperty()
+  @Column({ unique: true })
+  username: string;
 
-    @ApiProperty()
-    @Column({ unique: true })
-    email: string;
+  @ApiProperty()
+  @Column({ unique: true })
+  email: string;
 
-    @ApiProperty()
-    @Column()
-    password: string;
+  @ApiProperty()
+  @Column()
+  password: string;
 
-    @BeforeInsert()
-    @BeforeUpdate()
-    async hashPassword(password: string) {
-        const salt = await bcrypt.genSalt();
-        this.password = await bcrypt.hash(password || this.password, salt);
-    }
+  @BeforeInsert()
+  @BeforeUpdate()
+  async hashPassword(password: string) {
+    const salt = await bcrypt.genSalt();
+    this.password = await bcrypt.hash(password || this.password, salt);
+  }
 
-    @BeforeInsert()
-    emailToLowerCase() {
-        this.email = this.email.toLowerCase();
-    }
+  @BeforeInsert()
+  emailToLowerCase() {
+    this.email = this.email.toLowerCase();
+  }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -9,77 +9,85 @@ import { User } from './entities/user.entity';
 
 @Injectable()
 export class UsersService {
-    constructor(@InjectRepository(User) private usersRepository: Repository<User>, private authService: AuthService) { }
+  constructor(
+    @InjectRepository(User) private usersRepository: Repository<User>,
+    private authService: AuthService,
+  ) {}
 
-    getAll(): Promise<User[]> {
-        return this.usersRepository.find();
-        // SELECT * from user
-    }
+  getAll(): Promise<User[]> {
+    return this.usersRepository.find();
+    // SELECT * from user
+  }
 
-    async getOneById(id: number): Promise<User> {
-        try {
-            // const user = await this.usersRepository.findOneOrFail({ id }, { select: ['id', 'email', 'name'] });
-            const user = await this.usersRepository.findOneOrFail({ id });
-            return user;
-        } catch (err) {
-            throw err;
-        }
+  async getOneById(id: number): Promise<User | null> {
+    try {
+      // const user = await this.usersRepository.findOneOrFail({ id }, { select: ['id', 'email', 'name'] });
+      const user = await this.usersRepository.findOneOrFail({ id });
+      return user;
+    } catch (err) {
+      return null;
     }
+  }
 
-    async getOneByUsername(username: string): Promise<User> {
-        try {
-            const user = await this.usersRepository.findOneOrFail({ username });
-            return user;
-        } catch (err) {
-            throw err;
-        }
+  async getOneByUsername(username: string): Promise<User | null> {
+    try {
+      const user = await this.usersRepository.findOneOrFail({ username });
+      return user;
+    } catch (err) {
+      return null;
     }
+  }
 
-    async getOneByEmail(email: string): Promise<User> {
-        try {
-            const user = await this.usersRepository.findOneOrFail({ email });
-            return user;
-        } catch (err) {
-            throw err;
-        }
+  async getOneByEmail(email: string): Promise<User | null> {
+    try {
+      const user = await this.usersRepository.findOneOrFail({ email });
+      return user;
+    } catch (err) {
+      return null;
     }
+  }
 
-    async createUser(createUserDto: CreateUserDto): Promise<User> {
-        const user = this.getOneByEmail(createUserDto.email);
-        if (user) {
-            throw new HttpException('Email already in use', HttpStatus.CONFLICT);
-        } else {
-            const newUser = this.usersRepository.create(createUserDto);
-            return await this.usersRepository.save(newUser);
-        }
+  async createUser(createUserDto: CreateUserDto): Promise<User> {
+    const user = this.getOneByEmail(createUserDto.email);
+    if (user) {
+      throw new HttpException('Email already in use', HttpStatus.CONFLICT);
+    } else {
+      const newUser = this.usersRepository.create(createUserDto);
+      return await this.usersRepository.save(newUser);
     }
-    // Todo: make sure that user does not use the same password as before
-    async updateUser(userId: number, updateUserDto: UpdateUserDto): Promise<any> {
-        const user = await this.getOneById(userId);
-        if (user) {
-            return await this.usersRepository.save(Object.assign(user, updateUserDto));
-        } else {
-            throw new HttpException('User not found', HttpStatus.NOT_FOUND);
-        }
+  }
+  // Todo: make sure that user does not use the same password as before
+  async updateUser(userId: number, updateUserDto: UpdateUserDto): Promise<any> {
+    const user = await this.getOneById(userId);
+    if (user) {
+      return await this.usersRepository.save(
+        Object.assign(user, updateUserDto),
+      );
+    } else {
+      throw new HttpException('User not found', HttpStatus.NOT_FOUND);
     }
+  }
 
-    async deleteUser(id: number): Promise<User> {
-        const user = await this.getOneById(id);
-        return await this.usersRepository.remove(user);
-    }
+  async deleteUser(id: number): Promise<User> {
+    const user = await this.getOneById(id);
+    return await this.usersRepository.remove(user);
+  }
 
-    async login(loginUserDto: LoginUserDto): Promise<string> {
-        const user = await this.getOneByUsername(loginUserDto.username);
-        if (user) {
-            const valid = this.authService.comparePassword(loginUserDto.password, user.password);
-            if (valid) {
-                const userId = await this.getOneById(user.id);
-                return this.authService.generateJwt(userId);
-            } else {
-                throw new HttpException('Incorrect password', HttpStatus.UNAUTHORIZED);
-            }
-        } else {
-            throw new HttpException('User not found', HttpStatus.NOT_FOUND);
-        }
+  async login(loginUserDto: LoginUserDto): Promise<string> {
+    const user = await this.getOneByUsername(loginUserDto.username);
+    if (user) {
+      const valid = await this.authService.comparePassword(
+        loginUserDto.password,
+        user.password,
+      );
+      if (valid) {
+        const userId = await this.getOneById(user.id);
+        return this.authService.generateJwt(userId);
+      } else {
+        throw new HttpException('Incorrect password', HttpStatus.UNAUTHORIZED);
+      }
+    } else {
+      throw new HttpException('User not found', HttpStatus.NOT_FOUND);
     }
+  }
 }
